ff7728050ced0cede98997dd734a0fefdabd418c
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-03-18 08:42:10 -0600

    Refactor code.

--- js/auriga.js
+++ js/auriga.js
@@ -2,59 +2,20 @@
   Auriga
   ======*/
 function Auriga(container_id) {
-  var self = this;
-  var config = {
+  this._config = {
     width: 1200,
     height: 1000,
     max_speed: 80,
     err_tolerance: 3,
   };
 
-  this._create_shape();
-  this._layer = new Kinetic.Layer();
-  this._layer.add(this._shape);
-  this._stage = new Kinetic.Stage(container_id, config.width, config.height);
-  this._stage.add(this._layer);
-
-  var last_mouse_position = new Vector(self._shape.x, self._shape.y);
-  this._stage.on('mousemove', function(evt) {
-    var total_offset_x = 0;
-    var total_offset_y = 0;
-    var elem = evt.target;
-
-    do {
-      total_offset_x += elem.offsetLeft;
-      total_offset_y += elem.offsetTop;
-    } while(elem = elem.offsetParent);
-
-    last_mouse_position.x = evt.pageX - total_offset_x;
-    last_mouse_position.y = evt.pageY - total_offset_y;
-  });
-
-  this._stage.onFrame(function(frame) {
-    var position = new Vector(self._shape.x, self._shape.y);
-    var delta = last_mouse_position.sub(position);
-    delta.y = -delta.y;
-    if(delta.magnitude() < config.err_tolerance)
-      return;
-    console.log(delta.magnitude());
-
-    self._shape.acceleration = delta;
-    self._shape.velocity = self._shape.velocity.add(self._shape.acceleration);
-
-    var frame_max_speed = config.max_speed * (delta.magnitude() / config.width);
-    if(self._shape.velocity.magnitude() > frame_max_speed)
-      self._shape.velocity = self._shape.velocity.normalize().mult(frame_max_speed);
-
-    self._shape.x += self._shape.velocity.x;
-    self._shape.y -= self._shape.velocity.y;
-    self._layer.draw();
-  });
-
+  this._initialize_shapes(container_id);
+  this._configure_mouse_follower();
+  this._configure_redraw();
   this._stage.start();
 }
 
-Auriga.prototype._create_shape = function() {
+Auriga.prototype._initialize_shapes = function(container_id) {
   var shape = new Kinetic.Circle({
     x: 50,
     y: 50,
@@ -66,6 +27,52 @@ Auriga.prototype._create_shape = function() {
   shape.velocity = new Vector(0, 0);
   shape.acceleration = new Vector(0, 0.0);
   this._shape = shape;
+
+  this._layer = new Kinetic.Layer();
+  this._layer.add(this._shape);
+  this._stage = new Kinetic.Stage(container_id, this._config.width, this._config.height);
+  this._stage.add(this._layer);
+}
+
+Auriga.prototype._configure_mouse_follower = function() {
+  this._last_mouse_position = new Vector(this._shape.x, this._shape.y);
+  var self = this;
+  this._stage.on('mousemove', function(evt) {
+    var total_offset_x = 0;
+    var total_offset_y = 0;
+    var elem = evt.target;
+
+    do {
+      total_offset_x += elem.offsetLeft;
+      total_offset_y += elem.offsetTop;
+    } while(elem = elem.offsetParent);
+
+    self._last_mouse_position.x = evt.pageX - total_offset_x;
+    self._last_mouse_position.y = evt.pageY - total_offset_y;
+  });
+}
+
+Auriga.prototype._configure_redraw = function() {
+  var self = this;
+  this._stage.onFrame(function(frame) {
+    var position = new Vector(self._shape.x, self._shape.y);
+    var delta = self._last_mouse_position.sub(position);
+    delta.y = -delta.y;
+    if(delta.magnitude() < self._config.err_tolerance)
+      return;
+    console.log(delta.magnitude());
+
+    self._shape.acceleration = delta;
+    self._shape.velocity = self._shape.velocity.add(self._shape.acceleration);
+
+    var frame_max_speed = self._config.max_speed * (delta.magnitude() / self._config.width);
+    if(self._shape.velocity.magnitude() > frame_max_speed)
+      self._shape.velocity = self._shape.velocity.normalize().mult(frame_max_speed);
+
+    self._shape.x += self._shape.velocity.x;
+    self._shape.y -= self._shape.velocity.y;
+    self._layer.draw();
+  });
 }
 
 
